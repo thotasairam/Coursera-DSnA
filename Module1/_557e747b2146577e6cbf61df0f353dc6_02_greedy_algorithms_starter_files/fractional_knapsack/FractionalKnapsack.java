58f1341a1a7919538ca1280f158dbd112598732f
Sai Ram Thota <thotasairam@gmail.com>
2017-06-16 19:59:30 -0400

    Completed Fractional Knap sack

--- Module1/_557e747b2146577e6cbf61df0f353dc6_02_greedy_algorithms_starter_files/fractional_knapsack/FractionalKnapsack.java
+++ Module1/_557e747b2146577e6cbf61df0f353dc6_02_greedy_algorithms_starter_files/fractional_knapsack/FractionalKnapsack.java
@@ -1,9 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
+    private static double getOptimalValue(int capacity, ArrayList<MyValues> myValues) {
         double value = 0;
-        //write your code here
+
+        //Sorting values and weights according to value per weight
+        Collections.sort(myValues, new valuesPerWeightComparator());
+
+        for(int i = 0; i < myValues.size(); i++) {
+            if(capacity == 0){
+                return value;
+            }
+            else {
+                if(capacity > myValues.get(i).weight){
+                    value = value + myValues.get(i).values;
+                    capacity = capacity - myValues.get(i).weight;
+                }
+                else {
+                    double multiplier = (double) capacity/myValues.get(i).weight;
+                    value = value + (myValues.get(i).values * multiplier);
+                    capacity = 0;
+                }
+            }
+        }
 
         return value;
     }
@@ -12,12 +31,30 @@ public class FractionalKnapsack {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int capacity = scanner.nextInt();
-        int[] values = new int[n];
-        int[] weights = new int[n];
+        ArrayList<MyValues> myValues = new ArrayList<MyValues>();
+
         for (int i = 0; i < n; i++) {
-            values[i] = scanner.nextInt();
-            weights[i] = scanner.nextInt();
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            myValues.add(new MyValues(a, b, (double)a/b));
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        System.out.println(getOptimalValue(capacity, myValues));
     }
 } 
+
+
+class valuesPerWeightComparator implements Comparator<MyValues>{
+
+    @Override
+    public int compare(MyValues o1, MyValues o2) {
+        if(o1.vPW > o2.vPW){
+            return -1;
+        }
+        else if (o1.vPW < o2.vPW){
+                return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
