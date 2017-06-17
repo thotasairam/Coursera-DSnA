d94e22f77808363acfd59c3b99ad80e2806f8245
Sai Ram Thota <thotasairam@gmail.com>
2017-06-16 20:06:00 -0400

    Debugged string errors for single digit input

--- Module1/_557e747b2146577e6cbf61df0f353dc6_02_greedy_algorithms_starter_files/change/Change.java
+++ Module1/_557e747b2146577e6cbf61df0f353dc6_02_greedy_algorithms_starter_files/change/Change.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 public class Change {
     private static int getChange(int m) {
         String in = m + "";
+        int result = 0;
         String units, tens;
 
         if(in.length()>1) {
             tens = in.substring(0, in.length() - 1);
             units = in.substring(in.length() - 1);
-            m = 0 + Integer.parseInt(tens);
+            result = 0 + Integer.parseInt(tens);
         }
         else {
             units = in;
         }
 
         if(Integer.parseInt(units) >= 5){
-            m = m + 1 + (Integer.parseInt(units)%5);
+            result = result + 1 + (Integer.parseInt(units)%5);
         }
         else{
-            m = m + Integer.parseInt(units);
+            result = result + Integer.parseInt(units);
         }
 
-        return m;
+        return result;
     }
 
     public static void main(String[] args) {
