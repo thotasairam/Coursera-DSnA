04480aab4053ccac11dab725d3930b8dbd535f5b
Sai Ram Thota <thotasairam@gmail.com>
2017-06-21 20:07:47 -0400

    Completed Binary Search Recursively

--- Module1/Divide_and_conquer/binary_search/BinarySearch.java
+++ Module1/Divide_and_conquer/binary_search/BinarySearch.java
@@ -3,9 +3,19 @@ import java.util.*;
 
 public class BinarySearch {
 
-    static int binarySearch(int[] a, int x) {
-        int left = 0, right = a.length;
-        //write your code here
+    static int binarySearch(int[] a, int x, int left, int right) {
+
+        int mid = (int) Math.floor((left+right)/2);
+
+        if(left >= right){
+            return -1;
+        }else if(a[mid] == x){
+            return mid;
+        }else if(x > a[mid]){
+            return binarySearch(a,x, mid+1,right);
+        }else if (x < a[mid]){
+            return binarySearch(a,x,left,mid);
+        }
 
         return -1;
     }
@@ -31,7 +41,8 @@ public class BinarySearch {
         }
         for (int i = 0; i < m; i++) {
             //replace with the call to binarySearch when implemented
-            System.out.print(linearSearch(a, b[i]) + " ");
+            //System.out.print(linearSearch(a, b[i]) + " ");
+            System.out.print(binarySearch(a,b[i],0,a.length-1) + " ");
         }
     }
     static class FastScanner {
